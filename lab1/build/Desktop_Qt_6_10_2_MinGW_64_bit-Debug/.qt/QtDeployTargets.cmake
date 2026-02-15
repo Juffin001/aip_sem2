f7ab60cf1f2d193e4d1176f9d66d163ba20bb523
Juffin001 <magdeeviliaz24@yandex.ru>
2026-02-15 20:04:31 +0300

    1

---
+++ lab1/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_lab1_FILE C:/Users/79053/lrn fast/aip_2_sem/lab1_files/lab1/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/lab1.exe)
+set(__QT_DEPLOY_TARGET_lab1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_lab1_RUNTIME_DLLS C:/Users/79053/Documents/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Users/79053/Documents/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Users/79053/Documents/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
---
+++ lab1_2/dialogex2.h
@@ -0,0 +1,13 @@
+#ifndef DIALOGEX2_H
+#define DIALOGEX2_H
+#include <QDialog>
+#include "ui_ex2.h" // заголовок сгенерированный uic автоматически
+class DialogEx2 : public QDialog, public Ui::DialogEx2
+{
+    Q_OBJECT
+public:
+    DialogEx2( QWidget * parent = 0);
+private slots:
+    void onExitClicked();
+};
+#endif // DIALOGEX2_H
